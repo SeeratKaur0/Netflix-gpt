d239b7b768d67592d7a1970134755764b4cf3928
Seerat <seerat@Seerats-MacBook-Air.local>
2024-03-31 23:05:12 +0530

    code

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="font-bold text-3xl text-green-600">
+      Namaste Everyone let's build Netflix GPT
+    </div>
+  );
+}
+
+export default App;
